1f74b48ec9369d43158a7b00212ded604240a3f2
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2026-01-15 11:12:50 +0200

    chore: initial commit

---
+++ backend/prisma/migrations/20260115082811_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "TimeEntry" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "date" DATETIME NOT NULL,
+    "project" TEXT NOT NULL,
+    "hours" REAL NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
